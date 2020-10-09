0e9aa19710e72bf9376b8ad80f7024c97892d2a7
unknown <pipe.barreto07@gamil.com>
2020-10-09 11:46:39 -0500

    Tarea integradora 2

---
+++ MSC/src/ui/Main.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class Main{
+	
+	
+	public static void main(String [] args){
+		
+		
+		System.out.println("(((((((((((((((((((((((((((((((((((((((((((((((((\n"+ 
+		                   "((((((((((((((((/((((((((((((((((((((((((((((((((");
+				
+	}
+}
